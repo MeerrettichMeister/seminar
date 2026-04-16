f0a2588d837c863be9517b0af3ba02035b5cb25e
MeerrettichMeister <froesenico@gmail.com>
2026-04-16 18:28:38 +0200

    Setup alternative type with Readonly<> typing instead of keyword

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ type AccessorCode = (typeof ALL_ACCESSORS)[number]
 
 type PartialDict<T> = Partial<{ [key in AccessorCode]: T }>
 
+type Extended<T> = {flag: boolean, value: T}
+
 type WriteLocked<Type> = { + readonly [Property in keyof Type]: Type[Property] extends {} ? WriteLocked<Type[Property]> : Type[Property] }
 
+type AltWriteLocked<Type> = {[Property in keyof Type]: Type[Property] extends {} ? AltWriteLocked<Type[Property]> : Readonly<Type[Property]> }
 
 class FunkyClass {
 
@@ -24,14 +27,20 @@ interface Composition extends Coordinates {
     params: PartialDict<boolean>
 }
 
-function lockInterface<T>(element: T): WriteLocked<T> {
-    return element as WriteLocked<T>
-}
-
 interface Giga {
     composed: Composition;
     obj: FunkyClass;
 }
+
+function lockInterface<T>(element: T): WriteLocked<T> {
+    return element as WriteLocked<T>
+}
+
+function altLockInterface<T>(element: T): AltWriteLocked<T> {
+    return element as AltWriteLocked<T>
+}
+
+
 
 
 const myCoord1: Coordinates = {x: 1, y: 2}
@@ -48,4 +57,6 @@ const gigaObject: Giga = {
 const locked = lockInterface(myCoord1);
 const lockedBig = lockInterface(biggerObject)
 const lockedGiga = lockInterface(gigaObject);
-const foo = locked.x
+const altLocked = altLockInterface(lockedBig);
+altLocked.x = 5
+
